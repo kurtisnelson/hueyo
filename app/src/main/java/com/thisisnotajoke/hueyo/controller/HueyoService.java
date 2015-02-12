2ecbee560873cf731b0d7f2d26473520c953d534
Mohamed Moubarak <mohd.moubarak@gmail.com>
2015-02-12 17:44:25 +0200

    removed reflection

--- app/src/main/java/com/thisisnotajoke/hueyo/controller/HueyoService.java
+++ app/src/main/java/com/thisisnotajoke/hueyo/controller/HueyoService.java
@@ -1,7 +1,6 @@
 package com.thisisnotajoke.hueyo.controller;
 
 import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
@@ -35,7 +34,6 @@ import com.thisisnotajoke.hueyo.model.myo.MyoEvent;
 import com.thisisnotajoke.hueyo.model.myo.PoseConsumer;
 import com.thisisnotajoke.hueyo.model.myo.PoseEvent;
 
-import java.lang.reflect.Method;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -44,21 +42,6 @@ public class HueyoService extends Service {
     private static final String TAG = "HueyoService";
 	private static final int ONGOING_NOTIFICATION_ID = 101;
     private final IBinder mBinder = new LocalBinder();
-
-	private static final Class<?>[] mSetForegroundSignature = new Class[] {
-	    boolean.class};
-	private static final Class<?>[] mStartForegroundSignature = new Class[] {
-	    int.class, Notification.class};
-	private static final Class<?>[] mStopForegroundSignature = new Class[] {
-	    boolean.class};
-		
-	private NotificationManager mNM;
-	private Method mSetForeground;
-	private Method mStartForeground;
-	private Method mStopForeground;
-	private Object[] mSetForegroundArgs = new Object[1];
-	private Object[] mStartForegroundArgs = new Object[2];
-	private Object[] mStopForegroundArgs = new Object[1];
 		
     @Inject
     protected PreferenceUtil mPrefUtils;
@@ -78,79 +61,10 @@ public class HueyoService extends Service {
             return HueyoService.this;
         }
     }
-
-	void invokeMethod(Method method, Object[] args) {
-	    try {
-	        method.invoke(this, args);
-	    } catch (java.lang.reflect.InvocationTargetException e) {
-	        // Should not happen.
-	        Log.w(TAG, "Unable to invoke method", e);
-	    } catch (IllegalAccessException e) {
-	        // Should not happen.
-	        Log.w(TAG, "Unable to invoke method", e);
-	    }
-	}
-	
-	/**
-	 * This is a wrapper around the new startForeground method, using the older
-	 * APIs if it is not available.
-	 */
-	void startForegroundCompat(int id, Notification notification) {
-	    // If we have the new startForeground API, then use it.
-	    if (mStartForeground != null) {
-	        mStartForegroundArgs[0] = Integer.valueOf(id);
-	        mStartForegroundArgs[1] = notification;
-	        invokeMethod(mStartForeground, mStartForegroundArgs);
-	        return;
-	    }
-
-	    // Fall back on the old API.
-	    mSetForegroundArgs[0] = Boolean.TRUE;
-	    invokeMethod(mSetForeground, mSetForegroundArgs);
-	    mNM.notify(id, notification);
-	}
-	
-	/**
-	 * This is a wrapper around the new stopForeground method, using the older
-	 * APIs if it is not available.
-	 */
-	void stopForegroundCompat(int id) {
-	    // If we have the new stopForeground API, then use it.
-	    if (mStopForeground != null) {
-	        mStopForegroundArgs[0] = Boolean.TRUE;
-	        invokeMethod(mStopForeground, mStopForegroundArgs);
-	        return;
-	    }
-
-	    // Fall back on the old API.  Note to cancel BEFORE changing the
-	    // foreground state, since we could be killed at that point.
-	    mNM.cancel(id);
-	    mSetForegroundArgs[0] = Boolean.FALSE;
-	    invokeMethod(mSetForeground, mSetForegroundArgs);
-	}
 	
     @Override
     public void onCreate() {
         super.onCreate();
-		
-		mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-	    try {
-	        mStartForeground = getClass().getMethod("startForeground",
-	                mStartForegroundSignature);
-	        mStopForeground = getClass().getMethod("stopForeground",
-	                mStopForegroundSignature);
-	        return;
-	    } catch (NoSuchMethodException e) {
-	        // Running on an older platform.
-	        mStartForeground = mStopForeground = null;
-	    }
-	    try {
-	        mSetForeground = getClass().getMethod("setForeground",
-	                mSetForegroundSignature);
-	    } catch (NoSuchMethodException e) {
-	        throw new IllegalStateException(
-	                "OS doesn't have Service.startForeground OR Service.setForeground!");
-	    }
 		
         HandlerThread thread = new HandlerThread("HueyoService");
         thread.start();
@@ -165,14 +79,14 @@ public class HueyoService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-		
+
 		Notification notification = new Notification(R.drawable.ic_launcher, getText(R.string.ticker_text),
 		        System.currentTimeMillis());
 		Intent notificationIntent = new Intent(this, HueyoService.class);
 		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
 		notification.setLatestEventInfo(this, getText(R.string.notification_title),
 		        getText(R.string.notification_message), pendingIntent);
-		startForegroundCompat(ONGOING_NOTIFICATION_ID, notification);
+		startForeground(ONGOING_NOTIFICATION_ID, notification);
 		
         return START_STICKY;
     }
@@ -182,7 +96,7 @@ public class HueyoService extends Service {
         EventBusUtils.unregister(this);
 
 		// Make sure our notification is gone.
-		stopForegroundCompat(ONGOING_NOTIFICATION_ID);
+		stopForeground(true);
 		
         destroyMyo();
         destroyHue();
